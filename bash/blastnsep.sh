e768132e74267dd344bc99f9d8db0787b1a360b1
Roberto Rosati <robrosit@gmail.com>
2020-04-01 13:23:42 -0300

    Support for flexible output file names

--- bash/blastnsep.sh
+++ bash/blastnsep.sh
@@ -1,20 +1,21 @@
 echo "BLAST program: $1"
 echo "BLAST database name: $2"
 echo "Reference sequence: $3"
-echo "Minimum match length: $4"
-echo "Other BLAST options: $5"
-echo "Other Python options: $6"
+echo "Reference name: $4"
+echo "Minimum match length: $5"
+echo "Other BLAST options: $6"
+echo "Other Python options: $7"
 
 echo "Finding matches and opening HTML output..."
-$1 -db $2 -query $3 -html -out $1.html -num_descriptions 99999 -num_alignments 99999 $5
-firefox $1.html
+$1 -db $2 -query $3 -html -out $1_$4.html -num_descriptions 99999 -num_alignments 99999 $6
+firefox $1_$4.html
 
 echo "Creating tabular output..."
-$1 -db $2 -query $3 -out $1_tab.out -num_alignments 99999 -outfmt 6 $5
+$1 -db $2 -query $3 -out $1_$4_tab.out -num_alignments 99999 -outfmt 6 $6
 # This is equal to -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore"
 # Add headers
-echo -e "qseqid\tsseqid\tpident\tlength\tmismatch\tgapopen\tqstart\tqend\tsstart\tsend\tevalue\tbitscore" | cat - $1_tab.out > $1_tab.tsv
-rm $1_tab.out
-echo "Separating the sequencess of match length > $4..."
-python3 ../python/fromfasta.py -i ./$2.fa -o $1_selected.fa -b ./$1_tab.tsv "3 >= $4 1" $6
+echo -e "qseqid\tsseqid\tpident\tlength\tmismatch\tgapopen\tqstart\tqend\tsstart\tsend\tevalue\tbitscore" | cat - $1_$4_tab.out > $1_$4_tab.tsv
+rm $1_$4_tab.out
+echo "Separating the sequencess of match length > $5..."
+python3 ../python/fromfasta.py -i ./$2.fa -o $1_$4_selected.fa -b ./$1_$4_tab.tsv "3 >= $5 1" $7
 echo "blastnsep DONE!"
--- commandline.sh
+++ commandline.sh
@@ -2,6 +2,7 @@
 # Save it in NCBI_seqs as: 'spike_YP_009724390.1.fa'
 
 PROTSEQ="../NCBI_seqs/spike_YP_009724390.1.fa"
+NAME="spike"
 
 # Download protein sequences as multiFASTA from: https://www.ncbi.nlm.nih.gov/labs/virus/vssi/#/virus?SeqType_s=Protein&VirusLineage_ss=Severe%20acute%20respiratory%20syndrome%20coronavirus%202%20(SARS-CoV-2),%20taxid:2697049
 #Save them in the NCBI_seqs directory as: 'NCBI_protein_30mar20_2645prots_sequences.fasta'
@@ -11,13 +12,14 @@ mkdir blast_ncbi
 cd blast_ncbi
 bash ../bash/dodb.sh ../NCBI_seqs/NCBI_protein_30mar20_2645prots_sequences.fasta prot ncbiprot
 
+
 # Find matches to spike protein
 # In Python, '--p True' purge (remove) sequences with indetermined aa's or N's
-bash ../bash/blastnsep.sh blastp ncbiprot $PROTSEQ 1000 '' '-p True'
+bash ../bash/blastnsep.sh blastp ncbiprot $PROTSEQ $NAME 1000 '' '-p True'
 #232 sequences returned
 
 #Aligning with Clustal Omega (or use the website)
-clustalo -i blastp_selected.fa -o blastp_selected.aln --outfmt=clu --force
+clustalo -i blastp_$NAME'_selected.fa' -o blastp_$NAME'_selected.aln' --outfmt=clu --force
 #Open in Jalview, if opened from the website then save as .aln
 
 
@@ -37,9 +39,16 @@ bash ../bash/dodb.sh ../Gisaid_seqs/gisaid_cov2020_sequences-30mar20_HiCovOnly_2
 # Find matches to spike protein
 # WE NEED TO USE '-seg no' TO AVOID LOSING THE FIRST 11 AMINOACIDS!
 # In Python, '-t True -p True' will translate sequences and purge (remove) sequences with indetermined aa's or N's
-bash ../bash/blastnsep.sh tblastn gisnuc $PROTSEQ 1000 '-seg no' '-t True -p True'
+bash ../bash/blastnsep.sh tblastn gisnuc $PROTSEQ $NAME 1000 '-seg no' '-t True -p True'
 #1933 sequences returned
 
 #Aligning with Clustal Omega (or use the website)
-clustalo -i tblastn_selected.fa -o tblastn_selected.aln --outfmt=clu --force
+clustalo -i tblastn_$NAME'_selected.fa' -o tblastn_$NAME'_selected.aln' --outfmt=clu --force
 #Open in Jalview, if opened from the website then save as .aln
+
+# Compare with other species
+#mkdir interspecies
+#cd interspecies
+#cat ../NCBI_seqs/spike_YP_009724390.1.fa ../NCBI_seqs/SARS_spike_NP_828851.1 > CoV-2_vs_SARS.fa
+#clustalo -i ./CoV-2_vs_SARS.fa -o CoV-2_vs_SARS.aln --outfmt=clu --force
+
